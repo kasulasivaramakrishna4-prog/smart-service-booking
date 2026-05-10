562a1df662caf71492b53ca611b00a87ea132480
kasulasivaramakrishna4-prog <kasulasivaramakrishna4@gmail.com>
2026-05-10 10:55:18 +0530

    Connected frontend to live backend

--- Frontend/client/src/services/api.js
+++ Frontend/client/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://smart-service-booking-pvje.onrender.com";
 
 export async function signupUser(userData) {
   const response = await fetch(`${BASE_URL}/signup`, {
